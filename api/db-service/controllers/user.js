f106e6644e6fe42d7c91746fd87a9a57cdba75ce
mauroepce <mauricioeslainerpc@gmail.com>
2023-07-05 18:13:22 -0400

    frontend ready, register and login

--- api/db-service/controllers/user.js
+++ api/db-service/controllers/user.js
@@ -1,4 +1,5 @@
 const users = require("../models/users");
+const { storeVerificationToken } = require("../utils/verificationToken");
 
 /**
  * Register a user on the DB
@@ -20,6 +21,12 @@ const registerUserDB = async (req,res) => {
         const userData = req.body;
         const userCreated = await users.create(userData);
         userCreated.set("password", undefined, {strict: false});
+
+        // Generate verification token
+        const verificationToken = uuidv4();
+        
+        // store the verification token on DB
+        await storeVerificationToken(userCreated._id, verificationToken);       
        
         res.status(201).send(userCreated)
     } catch (error) {
@@ -32,28 +39,10 @@ const registerUserDB = async (req,res) => {
     }
 }
 
-const storeVerificationToken = async (req, res) => {
-    try {
-        // destructuring the data from the request body
-        const { userId, verificationToken } = req.body;
-
-        // Store the verification token on DB
-        const storeVerificationTokenData = await users.findByIdAndUpdate(userId, {verificationToken});
-
-        res.status(200).send(storeVerificationTokenData);
-    } catch (error) {
-        console.error(`Error while storing the verification token "user" on DB: ${error.message}`);
-            res.status(500).json({
-                error: {
-                    message: error,
-                },
-            });
-    }
-}
 
 
 
 module.exports = {
     registerUserDB,
-    storeVerificationToken
+
 }
